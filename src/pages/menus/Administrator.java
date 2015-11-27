a129d40c51785c9f58cc5f1e9ce3f16cefd59418
Thomas Kamp <thomas.kamp@hva.nl>
2015-11-27 13:01:08 +0100

    menu's toegevoegd

--- src/pages/menus/Administrator.java
+++ src/pages/menus/Administrator.java
@@ -5,10 +5,50 @@
  */
 package pages.menus;
 
+import UI.LuggageMenu;
+import UI.LuggageUI;
+import database.DatabaseManager;
+import java.util.concurrent.Callable;
+
 /**
  *
- * @author Alex
+ * @author Alex/Thomas
  */
 public class Administrator {
+    private LuggageUI UI;
+    private DatabaseManager db;
     
+    private static final user.Session USER = user.Session.getInstance();
+    
+    public Administrator(LuggageUI UI, DatabaseManager db){
+        this.UI = UI;
+        this.db = db;
+        
+        // Maak een nieuwe menu aan met de LuggageMenu class, deze is wel beetje buggy nog
+        LuggageMenu menu = new LuggageMenu(UI);
+        
+        // Maak nieuw element aan, met de text Search Database
+        // Active state is waar
+        // En weer op dezelfde methode een eventlistener
+        menu.addMenuItem("Users", true, (Callable) () -> {
+            pages.Users page = new pages.Users(UI, db);
+            return true;
+        });
+        
+        // Je snapt het idee wel, maar hier is de active state false
+        menu.addMenuItem("Backup", false, (Callable) () -> {
+            pages.BackupManager page = new pages.BackupManager(UI, db);
+            return true;
+        });
+
+        menu.addMenuItem("Logout", false, (Callable) () -> {
+            USER.logout();
+            UI.reset();
+            pages.Login page = new pages.Login(UI, db);
+            return true;
+        });
+        UI.setTop(
+            menu.toNode() // menu.getMenu geeft et menu terug
+        );
+    }
 }
--- src/pages/menus/Manager.java
+++ src/pages/menus/Manager.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pages.menus;
+
+import UI.LuggageMenu;
+import UI.LuggageUI;
+import database.DatabaseManager;
+import java.util.concurrent.Callable;
+import pages.*;
 
 /**
  *
- * @author Alex
+ * @author Alex/Thomas
  */
 public class Manager {
-    
+
+    private LuggageUI UI;
+    private DatabaseManager db;
+
+    private static final user.Session USER = user.Session.getInstance();
+
+    public Manager(LuggageUI UI, DatabaseManager db) {
+        this.UI = UI;
+        this.db = db;
+
+        // Maak een nieuwe menu aan met de LuggageMenu class, deze is wel beetje buggy nog
+        LuggageMenu menu = new LuggageMenu(UI);
+
+        // Maak nieuw element aan, met de text Search Database
+        // Active state is waar
+        // En weer op dezelfde methode een eventlistener
+        menu.addMenuItem("Logs", true, (Callable) () -> {
+            pages.Logs page = new pages.Logs(UI, db);
+            return true;
+        });
+
+        // Je snapt het idee wel, maar hier is de active state false
+        menu.addMenuItem("Locations", false, (Callable) () -> {
+            pages.Locations page = new pages.Locations(UI, db);
+            return true;
+        });
+
+        menu.addMenuItem("Stats", false, (Callable) () -> {
+            pages.Stats page = new pages.Stats(UI, db);
+            return true;
+        });
+        
+        menu.addMenuItem("Logout", false, (Callable) () -> {
+            USER.logout();
+            UI.reset();
+            pages.Login page = new pages.Login(UI, db);
+            return true;
+        });
+        UI.setTop(
+                menu.toNode() // menu.getMenu geeft et menu terug
+        );
+    }
 }
