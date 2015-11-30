94b1079a0d06860c929a86739d5ac8310089d491
Jimmy van Wieringen <jvw91@msn.com>
2015-11-30 14:38:57 +0100

    Begin F1 Screen

--- src/pages/F1Screen.java
+++ src/pages/F1Screen.java
@@ -8,6 +8,18 @@ package pages;
 import UI.LuggageUI;
 import database.DatabaseManager;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import UI.LuggageForm;
+import UI.LuggageTable;
+import UI.LuggageUI;
+import database.DatabaseManager;
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -15,8 +27,9 @@ import javafx.scene.layout.GridPane;
  * @author Alex
  */
 public class F1Screen {
+
     private LuggageUI UI;
-    private DatabaseManager db;
+    //private DatabaseManager db;  //Niet nodig voor F1 screen? - jvw
     public GridPane view = new GridPane();
 
     private static final user.Session USER = user.Session.getInstance();
@@ -24,8 +37,30 @@ public class F1Screen {
     public F1Screen(LuggageUI UI, DatabaseManager db) {
 
         this.UI = UI;
-        this.db = db;
+        // this.db = db; //Niet nodig voor F1 screen? - jvw
 
         view.setPadding(new Insets(50, 50, 50, 50));
+
+        UI.setTitle("Help");
+
+        GridPane test = new GridPane();
+        test.setPadding(new Insets(50, 50, 50, 50));
+
+        Label heading = UI.createHeading("HELP, Bagage zoeken.");
+
+        test.add(heading, 1, 1);
+        
+        LuggageForm form = new LuggageForm(UI);
+        form.addLabel("Email: ");
+        
+        
+        
+        
+        
+        
+        
+      
+        
+
     }
 }
