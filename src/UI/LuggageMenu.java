5e5fff5c39cc27fcec8cfb8fccc4932edad657a9
Alex Lisenkov <contact@alexej.nl>
2015-12-03 15:13:59 +0100

    Comments toegevoegd

--- src/UI/LuggageMenu.java
+++ src/UI/LuggageMenu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI;
 
 import java.util.concurrent.Callable;
@@ -23,7 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 
 /**
- *
+ * Creates a Menu
  * @author Alex
  */
 public class LuggageMenu {
