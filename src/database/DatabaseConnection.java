2e91676e898decf4299d63a438312e425ae18f05
Tom Scholten <tscholten97@gmail.com>
2016-01-19 14:21:28 +0100

    Mijn comments

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -25,35 +25,46 @@ public class DatabaseConnection {
     /**
      * Makes a connection to the database.
      *
-     * @throws SQLException SQL error exeptions
-     * @throws java.io.FileNotFoundException IO file error exeptions
-     * @throws java.io.IOException IO error exeptions
+     * @throws SQLException SQL error exceptions
+     * @throws java.io.FileNotFoundException IO file error exceptions
+     * @throws java.io.IOException IO error exceptions
      */
     public DatabaseConnection() throws SQLException, FileNotFoundException, IOException {
-        BufferedReader br = new BufferedReader(new FileReader(".env"));
+        BufferedReader br = new BufferedReader(new FileReader(".env")); //Reads a (text)file
         try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+            StringBuilder sb = new StringBuilder(); //builds strings
+            String line = br.readLine(); //Read the .env file per line
 
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
+                sb.append(line); //Adds a line to the stringbuilder
+                sb.append(System.lineSeparator()); //Adds a break to the Stringbuilder
+                line = br.readLine(); //Reads another line that will be added next
             }
-            String everything = sb.toString();
-            System.out.println(everything);
+            String everything = sb.toString(); //Total String of the .env file
+            System.out.println(everything); //Test in the console
             
-            Pattern h = Pattern.compile("HOST=(.+)");
+            /*
+            The regex is a tool to find specific text in a String. To find the text you use different
+            symbols, all these symboles have their own definition. The dot stands for everything except
+            the linebreaks, the plus stands for everything that comes after the thing I did before this,
+            except what comes after a linebreak. So Down here I say HOST=(.+), it takes everything 
+            after the "HOST=" because of the dot, the plus makes sure it stops at the linebreak. Since
+            the .env excist out of four lines with break between them, we can get the HOST, DATABASE,
+            USERNAME and PASSWORD all individually and save them in a variable.
+            */
+            
+            Pattern h = Pattern.compile("HOST=(.+)"); //These are the patterns
             Pattern d = Pattern.compile("DATABASE=(.+)");
             Pattern u = Pattern.compile("USERNAME=(.+)");
             Pattern p = Pattern.compile("PASSWORD=(.+)");
-            Matcher host = h.matcher(everything);
+            
+            Matcher host = h.matcher(everything); //Here it tells it with what is should be matched
             Matcher db = d.matcher(everything);
             Matcher user = u.matcher(everything);
             Matcher pass = p.matcher(everything);
 
             if (host.find()) {
-                this.host = host.group(1);
+                this.host = host.group(1); //Here it finds the match
             }
             if (db.find()) {
                 this.database = db.group(1);
@@ -65,23 +76,11 @@ public class DatabaseConnection {
                 this.password = pass.group(1);
             }
         } finally {
-            br.close();
+            br.close(); 
         }
-        
-        
-        
+        //Down here it uses the different variables to make a connection with the database.
         this.connection = DriverManager.getConnection("jdbc:mysql://"+ host +"/"+database, user, password);
 
-    }
-
-    /**
-     * Executes a query to insert into the database.
-     *
-     * @param query String query that needs to be executed
-     * @throws SQLException SQL error exeptions
-     */
-    public void executeQuery(String query) throws SQLException {
-        this.connection.createStatement().executeQuery(query);
     }
 
     /**
@@ -96,7 +95,8 @@ public class DatabaseConnection {
     }
 
     /**
-     *
+     * Used for inserting, deleting and updating the database with a query.
+     * 
      * @param query String query that needs to be executed
      * @throws SQLException SQL error exeptions
      */
